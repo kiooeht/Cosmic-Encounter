d1782ed8e4ba2192bc2065b776addfcb7069c906
Kiooeht <kiooeht@gmail.com>
2010-10-20 17:01:14 -0700

    begin macron - revealMath

---
+++ powers/macron.py
@@ -0,0 +1,10 @@
+from stuff.player import *
+
+class macron(player):
+  def revealMath(self, aV):
+    if aV[0] != "N":
+      aV[3] = aV[0]*4 + aV[1] + aV[2]
+    else:
+      aV[3] = "N"
+
+    return aV
