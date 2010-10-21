722a07e75dd6cd89cbae206e3eb4f7d801c6f735
Kiooeht <kiooeht@gmail.com>
2010-10-21 15:55:25 -0700

    check user input for stupidity

--- powers/hacker.py
+++ powers/hacker.py
@@ -5,14 +5,27 @@ from imports.globals import players
 class hacker(player):
   def getCompensation(self,plyr,n):
     print(self.name+">>")
-    printStats()
-    plyrNum = input("Who would you like to take compensation from? (number): ")
-    plyr = players[int(plyrNum)]
+    while 1:
+      printStats()
+      plyrNum = input("Who would you like to take compensation from? [0-"+str(len(players)-1)+"]: ")
+      if plyrNum.isdigit() and int(plyrNum) < len(players) and int(plyrNum) >= 0:
+        if players[int(plyrNum)] == self:
+          print("That is you. You can't compensate yourself.")
+        else:
+          plyr = players[int(plyrNum)]
+          break
+      else:
+        print("That player does not exist")
     for x in range(0, n):
-      plyr.showHand()
-      print("You can take "+ str(n-x) +" more cards")
-      crd = input("Select card to take [0-"+str(len(plyr.hand)-1)+"]: ")
-      self.getCard(plyr.giveCompensation(int(crd)))
+      while 1:
+        plyr.showHand()
+        print("You can take "+ str(n-x) +" more cards")
+        crd = input("Select card to take [0-"+str(len(plyr.hand)-1)+"]: ")
+        if crd.isdigit() and int(crd) < len(plyr.hand) and int(crd) >= 0:
+          self.getCard(plyr.giveCompensation(int(crd)))
+          break
+        else:
+          print("That does not exist in their hand")
 
   def giveCompensation(self, crd):
     while 1:
