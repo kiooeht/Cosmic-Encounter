b9e4ec0b69dfacb68825f5c0b79908339b2e840b
Kiooeht <kiooeht@gmail.com>
2010-10-22 11:02:12 -0700

    fixed weird non-matching indentation problem

--- powers/warpish.py
+++ powers/warpish.py
@@ -1,7 +1,7 @@
 from imports.player import *
 
 class warpish(player):
-	def revealMath(self, aV):
+  def revealMath(self, aV):
     if aV[0] != "N":
       aV[3] = aV[0] + aV[1] + aV[2] + sum([i for i in warp.values()])
     else:
