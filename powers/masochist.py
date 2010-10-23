41be3d1d37f8ddc7e92ba491b2abad1960a75139
Kiooeht <kiooeht@gmail.com>
2010-10-23 01:55:26 -0700

    fixed  "||" to "or"

--- powers/masochist.py
+++ powers/masochist.py
@@ -2,7 +2,7 @@ from imports.player import *
 
 class masochist(player):
   def checkWin(self):
-    if self.getColonies() >= 5 || self.getShipCount() <= 0:
+    if self.getColonies() >= 5 or self.getShipCount() <= 0:
       return True
     else:
       return False
