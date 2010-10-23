1419351571b07ed2b5babacd844369322f75f79f
Kiooeht <kiooeht@gmail.com>
2010-10-23 01:54:44 -0700

    draw systems before placing ships

--- powers/zombie.py
+++ powers/zombie.py
@@ -1,6 +1,8 @@
 from imports.player import *
+from imports.drawing import draw
 
 class zombie(player):
   def killShips(self, num, loc, locN):
     loc[locN] -= num
+    draw()
     self.placeShips(num)
