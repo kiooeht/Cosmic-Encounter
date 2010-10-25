0a23f1402c956d897782f777082c6ed275bd4505
Kiooeht <kiooeht@gmail.com>
2010-10-24 17:54:40 -0700

    added trader

---
+++ powers/trader.py
@@ -0,0 +1,15 @@
+from imports.player import *
+
+class trader(player):
+  def beforeCardsChosen(self, oppo):
+    while 1:
+      trade = input("Trade hands with other player? [y/n]: ")
+      if trade.lower() == "y":
+        spare = self.hand
+        self.hand = oppo.hand
+        oppo.hand = spare
+        return 1
+      elif trade.lower() == "n":
+        return 0
+      else:
+        print("ERROR: YOU FAIL")
