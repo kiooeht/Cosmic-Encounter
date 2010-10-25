08f53ad90c54834152914f2ebd366d8cc96cdbff
Kiooeht <kiooeht@gmail.com>
2010-10-24 18:05:11 -0700

    say user's name in prompt

--- powers/trader.py
+++ powers/trader.py
@@ -3,7 +3,7 @@ from imports.player import *
 class trader(player):
   def beforeCardsChosen(self, oppo):
     while 1:
-      trade = input("Trade hands with other player? [y/n]: ")
+      trade = input(self.name+">> Trade hands with other player? [y/n]: ")
       if trade.lower() == "y":
         spare = self.hand
         self.hand = oppo.hand
