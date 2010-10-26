767c415be16e53ae64dfb93ec01e6e8f93ddc335
Kiooeht <kiooeht@gmail.com>
2010-10-26 01:02:37 -0700

    rework mite to account for changes in choice

--- powers/mite.py
+++ powers/mite.py
@@ -8,17 +8,17 @@ class mite(player):
   def launch(self,dest):
     choice = super().launch(dest)
 
-    if len(dest.hand) > 3:
+    if len(choice[1].hand) > 3:
       while 1:
-        miteChoice = input(dest.name+">> Discard down to 3 cards or give "+self.name+" a colony? [3/c]: ")
+        miteChoice = input(choice[1].name+">> Discard down to 3 cards or give "+self.name+" a colony? [3/c]: ")
         if miteChoice == "3":
-          for x in range(0, len(dest.hand)-3):
+          for x in range(0, len(choice[1].hand)-3):
             random.seed(time.gmtime())
-            crd = int(random.random()*len(dest.hand))
-            dest.discardCard(crd)
+            crd = int(random.random()*len(choice[1].hand))
+            choice[1].discardCard(crd)
           break
         elif miteChoice.lower() == "c":
-          planet = dest.system.planet[int(choice)]
+          planet = dest.system.planet[int(choice[0])]
           self.colonize(planet, mothership[self])
           choice = "successful"
           break
