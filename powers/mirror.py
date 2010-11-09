3cadbaf3d11dde69ec6e52fd5f80b304d84dc516
Kiooeht <kiooeht@gmail.com>
2010-11-08 21:48:54 -0800

    updated for new either/and/or math override

--- powers/mirror.py
+++ powers/mirror.py
@@ -4,7 +4,6 @@ class mirror(player):
   def __init__(self, g, n, ident, pps, spp, crd):
     super().__init__(g, n, ident, pps, spp, crd)
     self.oppoRevealBool = True
-    self.mathOverride = False
 
   def rev(self, n):
     if n < 10:
@@ -25,10 +24,12 @@ class mirror(player):
       while 1:
         checkMirror = input(self.name+">> Would you like to mirror? [y/n]: ")
         if checkMirror.lower() == "n":
-          self.mathOverride = False
+          self.mathOverrideSelf = False
+          self.mathOverrideOppo = False
           return 0
         elif checkMirror.lower() == "y":
-          self.mathOverride = True
+          self.mathOverrideSelf = True
+          self.mathOverrideOppo = True
           return 0
         else:
           print("Whoa there, buddy! You need to input an answer in a correct format!")
