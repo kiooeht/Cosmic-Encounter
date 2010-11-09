def0953baa778ddd425236ab02d7edf61afb560e
Kiooeht <kiooeht@gmail.com>
2010-11-08 21:51:54 -0800

    updated antimatter for new game system

--- powers/antimatter.py
+++ powers/antimatter.py
@@ -3,7 +3,7 @@ from imports.player import *
 class antimatter(player):
   def __init__(self, g, n, ident, pps, spp, crd):
     super().__init__(g, n, ident, pps, spp, crd)
-    self.oppoRevealBool = True
+    self.mathOverrideOppo = True
 
   def revealMath(self, aV):
     if aV[0] != "N":
@@ -13,45 +13,10 @@ class antimatter(player):
 
     return aV
 
-  def oppoReveal(self,oppo,crd, pNum):
-    # [0] = card
-    # [1] = number of ships
-    # [2] = total allies
-    # [3] = total power
-    # [4] = hash of individual ally ship numbers
-    attackValue = [0]*5
-    if crd == 99:
-      attackValue[0] = "N"
-    else:
-      attackValue[0] = crd
-
-    attackValue[4] = {}
-
-    if pNum == -1:
-      aC = 0
-      for x in players:
-        if x == oppo:
-          attackValue[1] = mothership[x]
-        else:
-          aC += x.shipWorth(mothership[x])
-          attackValue[4][x] = mothership[x]
-      attackValue[2] = aC
-    else:
-      attackValue[1] = oppo.system.planet[int(pNum)].ships[oppo]
-      aC = 0
-      for x in players:
-        aC += x.shipWorth(carriership[x])
-        attackValue[4][x] = carriership[x]
-      attackValue[2] = aC
-
-    return self.oppoRevealMath(oppo, attackValue)
-
-  def oppoRevealMath(self, oppo, aV):
-    aV = oppo.revealMath(aV)
+  def powerMath(self, aV):
     if aV[0] != "N":
       aV[3] -= 2 * aV[2]
     else:
       aV[3] = "N"
 
     return aV
-
