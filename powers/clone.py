fa8690119cfaf1caa4fc093ed921ae2126d2cb94
Kiooeht <kiooeht@gmail.com>
2010-11-15 13:08:19 -0800

    fix clone to print N if using a negotiate

--- powers/clone.py
+++ powers/clone.py
@@ -2,8 +2,12 @@ from imports.player import *
 
 class clone(player):
   def discardUsedECard(self, crd):
+    if crd < 90:
+      printCrd = str(crd)
+    elif crd == 99:
+      printCrd = "N"
     while 1:
-      keep = input(self.name+">> Would you like to keep the encounter card ("+str(crd)+")? [Y/n]: ")
+      keep = input(self.name+">> Would you like to keep the encounter card ("+printCrd+")? [Y/n]: ")
       if keep.lower() == "y" or keep.lower() == "":
         self.getCard(crd)
         break
