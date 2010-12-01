51db0b00b1673abbab32a4ec744d4615e790cad8
Kiooeht <kiooeht@gmail.com>
2010-11-30 23:01:01 -0800

    remove superfluous print statements

--- imports/artifact.py
+++ imports/artifact.py
@@ -9,8 +9,6 @@ class artifact:
     plyr.discardCard(crd)
     work = True
     worked = [work]
-    print("BEFORE: "+str(worked[0]))
     for x in self.theGame.players:
       x.checkArtifacts("use card", self.name, worked)
-    print("AFTER: "+str(worked[0]))
     return worked[0]
